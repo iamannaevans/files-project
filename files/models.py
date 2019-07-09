a6af582acf8524e5cd14efcdb30c473168480b6a
Anna Evans <3.annaevans@gmail.com>
2019-07-09 21:31:35 +0200

    Add Files model

--- files/models.py
+++ files/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class File(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.TextField()
+    public = models.BooleanField(default=False)
+    file = models.FileField()
+
+    def __str__(self):
+        return self.name
--- files/views.py
+++ files/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import File
+from .serializers import FileSerializer
 
 # Create your views here.
+class FileView(viewsets.ModelViewSet):
+    queryset = File.objects.all()
+    serializer_class = FileSerializer
