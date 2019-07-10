f5410ea12c9e8d5d38d9d9a1b694a1ac57b15f6b
Anna Evans <3.annaevans@gmail.com>
2019-07-10 22:13:51 +0200

    Add delay

--- files/views.py
+++ files/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import File
 from .serializers import FileSerializer
+import time
+import random
 
 # Create your views here.
 class FileView(viewsets.ModelViewSet):
     queryset = File.objects.all()
     serializer_class = FileSerializer
+
+    def slow(self, request, *args, **kwargs):
+        time.sleep(random.randint(2, 20))
+        return super(FileView, self).dispatch(request, *args, **kwargs)
