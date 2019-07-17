f4d6f6f5f1ee431e0919b0b8ad20760f75bb2bbf
Anna Evans <3.annaevans@gmail.com>
2019-07-18 00:36:13 +0200

    Add test for successful file upload

--- files/tests.py
+++ files/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from .models import File
+from rest_framework import status
+from rest_framework.test import APITestCase
+import json
+import tempfile
 
-# Create your tests here.
+
+class FileTests(APITestCase):
+    def test_upload_file(self):
+        with tempfile.TemporaryDirectory() as temporary_dir:
+            with self.settings(MEDIA_ROOT=temporary_dir):
+                file = tempfile.NamedTemporaryFile()
+                file.write(b'This is a file.')
+                file.seek(0)
+
+                response = self.client.post('/files/', {
+                    'name': 'myfile',
+                    'description': 'this is my file',
+                    'public': True,
+                    'tags': json.dumps([]),
+                    'file': file
+                })
+
+                self.assertEqual(response.status_code, status.HTTP_200_OK)
+                self.assertEqual(File.objects.count(), 1)
+                self.assertEqual(File.objects.get().name, 'myfile')
